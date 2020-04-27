75a93dede4f6efd24eaf6f4fe67cdb3c62408b3b
RouvenGonzalez <rouvengonzalez@gmail.com>
2020-04-27 13:31:17 +0200

    deleted the redirect in login

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ function validate(){
    var rurl = window.location.href;
 
    // send data to git api https://github.com/login/oauth/authorize
-   var resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=https://software-developer-org.github.io/blog/titlepost/";
+   var resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158";
    window.location = resturl; 
 
     }

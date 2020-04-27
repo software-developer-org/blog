1f85986cd8222cef89e2c8d264cf0ad677393eeb
RouvenGonzalez <rouvengonzalez@gmail.com>
2020-04-27 11:31:49 +0200

    fixed the variable scope

--- login.js
+++ login.js
@@ -1,13 +1,14 @@
 function validate(){
 
    //get url for redirekt 
-   rurl= window.location.href;
+  const rurl= window.location.href;
 
    // generate state string and concat with http adress
 
 
    // send data to git api https://github.com/login/oauth/authorize
+   
+   const resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=${rurl}";
    window.location = resturl; 
-   resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=${rurl}";
 
     }

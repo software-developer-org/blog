1788c0c2f07616a9cd47abd28fcf0d0ba78146ff
RouvenGonzalez <rouvengonzalez@gmail.com>
2020-04-27 10:46:46 +0200

    change login function

--- login.js
+++ login.js
@@ -3,7 +3,12 @@ function validate(){
    //TO DOO:
    //random generate state string and concat with http adress
 
+   //get url for redirekt 
+   rurl = window.location.href;
+
+
    // send data to git api https://github.com/login/oauth/authorize
-   window.location = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158"
+   window.location = resturl; 
+   resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=${rurl}"
 
     }

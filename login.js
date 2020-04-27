890290d6fe0473e428da3dbb7f2b2ac3d9e1168f
RouvenGonzalez <rouvengonzalez@gmail.com>
2020-04-27 12:32:00 +0200

    redirekt with state string

--- login.js
+++ login.js
@@ -4,10 +4,10 @@ function validate(){
    var rurl = window.location.href;
 
    // generate state string and concat with http adress
-
+   var state64 = btoa(rurl);
 
    // send data to git api https://github.com/login/oauth/authorize
-   var resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=${rurl}";
+   var resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&state=${state64}&redirect_uri=${rurl}";
    window.location = resturl; 
 
     }

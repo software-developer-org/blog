649979d3c04af84dfa0e10ffe6c6b3acd4ce058a
RouvenGonzalez <rouvengonzalez@gmail.com>
2020-04-27 11:55:51 +0200

    rewrite variables for url call

--- login.js
+++ login.js
@@ -1,14 +1,13 @@
 function validate(){
 
    //get url for redirekt 
-  const rurl= window.location.href;
+   var rurl = window.location.href;
 
    // generate state string and concat with http adress
 
 
    // send data to git api https://github.com/login/oauth/authorize
-   
-   const resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=${rurl}";
-   window.location = resturl; 
+   var resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=";
+   window.location = resturl.concat(rurl); 
 
     }

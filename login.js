92c8f3952bc8af6eeb19440525cbe191136d1bf0
RouvenGonzalez <rouvengonzalez@gmail.com>
2020-04-27 13:21:51 +0200

    changed valdate function to onload

--- login.js
+++ login.js
@@ -3,11 +3,10 @@ function validate(){
    //get url for redirekt 
    var rurl = window.location.href;
 
-   // generate state string and concat with http adress
-   var state64 = btoa(rurl);
-
    // send data to git api https://github.com/login/oauth/authorize
-   var resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=${rurl}";
+   var resturl = "https://github.com/login/oauth/authorize?client_id=3cde25dedb0296247158&redirect_uri=https://software-developer-org.github.io/blog/titlepost/titlepost.html";
    window.location = resturl; 
 
     }
+
+document.onload = validate();

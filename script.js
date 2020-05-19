22caad50d421cab14f3e9de56cda8dc68541fa50
Ragip Gashi <ragipgashi@gmial.com>
2020-05-19 17:41:51 +0200

    Added dummy datas on click

---
+++ script.js
@@ -0,0 +1,42 @@
+function aboutFunction() {
+    let about = document.getElementById("about");
+    about.style.opacity = 0;
+
+    let teamM = document.getElementById("team");
+    teamM.style.opacity = 100;
+}
+
+function blogFunction() {
+    let blog = document.getElementById("summary");
+    blog.style.opacity = 0;
+
+    let blog1 = document.getElementById("fullpage");
+    blog1.style.opacity = 100;
+
+    let blog2 = document.getElementById("fullpage");
+    blog2.style.opacity = 100;
+
+    let blog3 = document.getElementById("fullpage");
+    blog3.style.opacity = 100;
+
+    let blog4 = document.getElementById("fullpage");
+    blog4.style.opacity = 100;
+
+    let blog5 = document.getElementById("fullpage");
+    blog5.style.opacity = 100;
+
+    let blog6 = document.getElementById("fullpage");
+    blog6.style.opacity = 100;
+
+    let blog7 = document.getElementById("fullpage");
+    blog7.style.opacity = 100;
+
+    let blog8 = document.getElementById("fullpage");
+    blog8.style.opacity = 100;
+
+    let blog9 = document.getElementById("fullpage");
+    blog9.style.opacity = 100;
+
+    let blog10 = document.getElementById("fullpage");
+    blog10.style.opacity = 100;
+}

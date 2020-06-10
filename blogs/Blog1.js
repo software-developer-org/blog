ef6bfb52db4cb66c585da1dcb1e2d4d65ac4b4f8
Ragip Gashi <44840806+RagipGashi@users.noreply.github.com>
2020-06-10 09:37:28 +0200

    Deleted Blog1.js

--- blogs/Blog1.js
+++
@@ -1,8 +0,0 @@
-{
-    title: "Blog 01 title";
-    id: "ID-01;
-    date: "2020-06-10";
-    author: "Ragip Gashi";
-    tldr: "To long don't read";
-    content: "This is the content from the first blog"
-}

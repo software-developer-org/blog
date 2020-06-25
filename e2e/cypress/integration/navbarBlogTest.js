ff2e01a15b3ecee24d142bc1500f71a96db141f8
RouvenGonzalez <rouvengonzalez@gmail.com>
2020-06-25 11:33:45 +0200

    changed some settings for eslint

--- e2e/cypress/integration/navbarBlogTest.js
+++ e2e/cypress/integration/navbarBlogTest.js
@@ -1,20 +1,19 @@
 describe('Testing blog navbar contract', () => {
-    beforeEach('direct to our local server', () => {
-        cy.visit('http://localhost:3000')
+  beforeEach('direct to our local server', () => {
+    cy.visit('http://localhost:3000');
+  });
 
-    })
+  it('check for navbar content', () => {
+    // Click on the Blog button
+    cy.get('#btnBlog').click();
+    // Click on the 1st Blog to see if it is empty or not
+    cy.get('#Blog1 > h4').click();
+    cy.get('#Blog1').should('not.be.empty');
 
-    it('check for navbar content', () => {
-        // Click on the Blog button
-        cy.get('#btnBlog').click();
-        // Click on the 1st Blog to see if it is empty or not
-        cy.get('#Blog1 > h4').click();
-        cy.get('#Blog1').should('not.be.empty');
+    cy.get('#Blog2 > h4').click();
+    cy.get('#Blog2').should('not.be.empty');
 
-        cy.get('#Blog2 > h4').click();
-        cy.get('#Blog2').should('not.be.empty');
-        
-        cy.get('#Blog3-Tai > h4').click();
-        cy.get('#Blog3').should('not.be.empty');
-    })
-})
+    cy.get('#Blog3-Tai > h4').click();
+    cy.get('#Blog3').should('not.be.empty');
+  });
+});

dd376f03111e7b329634a50164ab59f5289a0181
RouvenGonzalez <rouvengonzalez@gmail.com>
2020-07-30 17:00:36 +0200

    #44 fixed page format and added exceptions to the funktion calls in overview

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -39,99 +39,103 @@ function blogTitles() {
 
 function showBlogContent() {
   // Get the overviewElement with the ID we created in contract.js
-  const overviewElement = document.getElementById('blogSummary');
+  if (document.getElementById('blogSummary')) {
+    const overviewElement = document.getElementById('blogSummary');
 
-  // Append it to the overview block
+    // Append it to the overview block
 
-  showBlog().then((blog) => {
-    blog.forEach((blogpromise) => {
-      blogpromise.then((blogdata) => {
-        // Create a div for blog overview
-        const blogOverview = document.createElement('div');
-        blogOverview.setAttribute('id', 'blogOverview');
+    showBlog().then((blog) => {
+      blog.forEach((blogpromise) => {
+        blogpromise.then((blogdata) => {
+          // Create a div for blog overview
+          const blogOverview = document.createElement('div');
+          blogOverview.setAttribute('id', 'blogOverview');
 
-        const blogTitleDiv = document.createElement('div');
-        blogTitleDiv.setAttribute('id', 'Blog');
-        const blogAuthorDiv = document.createElement('div');
-        blogAuthorDiv.setAttribute('id', 'Author');
-        const blogDateDiv = document.createElement('div');
-        blogDateDiv.setAttribute('id', 'Date');
-        const blogContentDiv = document.createElement('div');
-        blogContentDiv.setAttribute('id', 'Content');
+          const blogTitleDiv = document.createElement('div');
+          blogTitleDiv.setAttribute('id', 'Blog');
+          const blogAuthorDiv = document.createElement('div');
+          blogAuthorDiv.setAttribute('id', 'Author');
+          const blogDateDiv = document.createElement('div');
+          blogDateDiv.setAttribute('id', 'Date');
+          const blogContentDiv = document.createElement('div');
+          blogContentDiv.setAttribute('id', 'Content');
 
-        // create child divs in overview div
-        blogOverview.appendChild(blogTitleDiv);
-        blogOverview.appendChild(blogAuthorDiv);
-        blogOverview.appendChild(blogDateDiv);
-        blogOverview.appendChild(blogContentDiv);
+          // create child divs in overview div
+          blogOverview.appendChild(blogTitleDiv);
+          blogOverview.appendChild(blogAuthorDiv);
+          blogOverview.appendChild(blogDateDiv);
+          blogOverview.appendChild(blogContentDiv);
 
-        const blogTitle = document.createTextNode(blogdata.title);
-        blogTitleDiv.appendChild(blogTitle);
-        const blogAutor = document.createTextNode(blogdata.author);
-        blogAuthorDiv.appendChild(blogAutor);
-        const blogDate = document.createTextNode(blogdata.date);
-        blogDateDiv.appendChild(blogDate);
-        const blogContent = document.createTextNode(blogdata.tldr);
-        blogContentDiv.appendChild(blogContent);
+          const blogTitle = document.createTextNode(blogdata.title);
+          blogTitleDiv.appendChild(blogTitle);
+          const blogAutor = document.createTextNode(blogdata.author);
+          blogAuthorDiv.appendChild(blogAutor);
+          const blogDate = document.createTextNode(blogdata.date);
+          blogDateDiv.appendChild(blogDate);
+          const blogContent = document.createTextNode(blogdata.tldr);
+          blogContentDiv.appendChild(blogContent);
 
-        overviewElement.appendChild(blogOverview);
+          overviewElement.appendChild(blogOverview);
 
-        blogOverview.onclick = () => {
-          showFullBlog(blogdata.title);
-        };
+          blogOverview.onclick = () => {
+            showFullBlog(blogdata.title);
+          };
+        });
       });
     });
-  });
+  }
 }
 // ---TODO-- id needs to be a parameter in full implementation
 async function showFullBlog() {
-  // get Json data of blog article
-  const blogArticleJson = await fetchBlogContent();
-  console.log(blogArticleJson);
+  if (document.getElementById('blogArticle')) {
+    // get Json data of blog article
+    const blogArticleJson = await fetchBlogContent();
+    console.log(blogArticleJson);
 
-  // create elements for the data
-  const titleElement = document.createElement('h1');
-  const authorElement = document.createElement('p');
-  const dateElement = document.createElement('p');
-  const tldrElement = document.createElement('div');
-  const contentElement = document.createElement('div');
-  const linkElement = document.createElement('div');
+    // create elements for the data
+    const titleElement = document.createElement('h1');
+    const authorElement = document.createElement('p');
+    const dateElement = document.createElement('p');
+    const tldrElement = document.createElement('div');
+    const contentElement = document.createElement('div');
+    const linkElement = document.createElement('div');
 
-  // write data into elements with paragraph logic
+    // write data into elements with paragraph logic
 
-  //---HEADER---
-  titleElement.innerHTML = blogArticleJson.title;
-  authorElement.innerHTML = blogArticleJson.author;
-  dateElement.innerHTML = blogArticleJson.date;
+    //---HEADER---
+    titleElement.innerHTML = blogArticleJson.title;
+    authorElement.innerHTML = blogArticleJson.author;
+    dateElement.innerHTML = blogArticleJson.date;
 
-  //---TLDR---
-  blogArticleJson.tldr.forEach((textString) => {
-    const paragraph = document.createElement('p');
-    paragraph.innerHTML = textString;
-    tldrElement.appendChild(paragraph);
-  });
+    //---TLDR---
+    blogArticleJson.tldr.forEach((textString) => {
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = textString;
+      tldrElement.appendChild(paragraph);
+    });
 
-  //---CONTENT---
-  blogArticleJson.content.forEach((textString) => {
-    const paragraph = document.createElement('p');
-    paragraph.innerHTML = textString;
-    contentElement.appendChild(paragraph);
-  });
-  //---LINKS---
-  blogArticleJson.links.forEach((textString) => {
-    const paragraph = document.createElement('p');
-    paragraph.innerHTML = textString;
-    linkElement.appendChild(paragraph);
-  });
+    //---CONTENT---
+    blogArticleJson.content.forEach((textString) => {
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = textString;
+      contentElement.appendChild(paragraph);
+    });
+    //---LINKS---
+    blogArticleJson.links.forEach((textString) => {
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = textString;
+      linkElement.appendChild(paragraph);
+    });
 
-  // get Element to display blog article and insert the created article elements
-  const blogDiv = document.getElementById('blogArticle');
-  blogDiv.appendChild(titleElement);
-  blogDiv.appendChild(authorElement);
-  blogDiv.appendChild(dateElement);
-  blogDiv.appendChild(tldrElement);
-  blogDiv.appendChild(contentElement);
-  blogDiv.appendChild(linkElement);
+    // get Element to display blog article and insert the created article elements
+    const blogDiv = document.getElementById('blogArticle');
+    blogDiv.appendChild(titleElement);
+    blogDiv.appendChild(authorElement);
+    blogDiv.appendChild(dateElement);
+    blogDiv.appendChild(tldrElement);
+    blogDiv.appendChild(contentElement);
+    blogDiv.appendChild(linkElement);
+  }
 }
 
 window.addEventListener('load', blogTitles);
